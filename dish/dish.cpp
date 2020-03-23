5573e3dabbebd03f054b97e67cca9133f9f792ef
Andrei Visoiu <visoiuandrei42@yahoo.ro>
2020-03-23 20:01:36 +0200

    nearly added dishes

---
+++ dish/dish.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Andrei on 3/20/2020.
+//
+
+#include "dish.h"
---
+++ dish/dish.h
@@ -0,0 +1,14 @@
+//
+// Created by Andrei on 3/20/2020.
+//
+
+#ifndef COOKBOOK_CI_DISH_H
+#define COOKBOOK_CI_DISH_H
+
+
+class dish {
+
+};
+
+
+#endif //COOKBOOK_CI_DISH_H
